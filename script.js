a8308ef55e2b98431daca6213601bb3dc255e257
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-17 01:26:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	firstName=document.getElementsByName('fname')[0].value;
+	lastName=document.getElementsByName('lname')[0].value;
+	alert(`${firstName} ${lastName}`)
 }
